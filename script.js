164404eb4f5088e8bcfe6fd4f0c3d84f11554bdc
Alex <alex.suarez244@gmail.com>
2020-08-04 18:42:24 +0000

    cambios

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.querySelector('.menu-boton').addEventListener('click', () =>  {
-    document.querySelector('.nav-menu').classList.toggle('show');
+document.querySelector(".menu-boton").addEventListener("click", () =>  {
+    document.querySelector(".nav-menu").classList.toggle("show");
 }); 
 
-ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.news-cards', {delay: 550 });
-ScrollReveal().reveal('.cards-banner-one', {delay: 550 });
-ScrollReveal().reveal('.cards-banner-two', {delay: 550 });
+ScrollReveal().reveal(".showcase");
+ScrollReveal().reveal(".news-cards", {delay: 550 });
+ScrollReveal().reveal(".cards-banner-one", {delay: 550 });
+ScrollReveal().reveal(".cards-banner-two", {delay: 550 });
