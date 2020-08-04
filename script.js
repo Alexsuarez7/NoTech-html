08cf7bf164d756e73c2314e07e264b856c7469fc
Alex <alex.suarez244@gmail.com>
2020-08-04 15:43:33 +0000

    agregando un delay a la animacion

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.querySelector('.menu-boton').addEventListener('click', () =>  {
     document.querySelector('.nav-menu').classList.toggle('show');
 }); 
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.headline'. {delay 550 });
